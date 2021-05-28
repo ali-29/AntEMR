011d8212e1a78ff5415576e0881d23d00a2c12cc
Ali <68532177+ali-29@users.noreply.github.com>
2021-05-28 17:47:52 +0700

    Add files via upload

---
+++ AntEMR/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using AntEMR.Views;
+using Xamarin.Forms;
+
+namespace AntEMR
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+
+            Routing.RegisterRoute(route: "login", typeof(LoginPage));
+            Routing.RegisterRoute(route: "registration", typeof(RegistrationPage));
+            Routing.RegisterRoute(route: "registrationdetails", typeof(RegistrationDetailsPage));
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
